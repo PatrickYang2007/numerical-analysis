ea641b8458ed5c7ee67d2cf0b8b391a258f439cf
Patrick <py235@cornell.edu>
2026-09-07 02:19:36 +0000

    Editing Problem 3b again

--- Problem Set 2/Problem3b.m
+++ Problem Set 2/Problem3b.m
@@ -1,15 +1,14 @@
 x = 1 - eps/2;
+goodN = [];
 badN = [];
-badP = [];
-badI = [];
 for n = 1:200
     delta = 1/n;
-    i = floor(x/delta);
-    p = x - delta*i;
-    if p < 0
+    p = x - delta*floor(x/delta);
+    if p >= 0
+        goodN(end+1) = n;
+    else
         badN(end+1) = n;
-        badP(end+1) = p;
-        badI(end+1) = i;
     end
 end
-count = numel(badN)
+numel(goodN)
+numel(badN)
